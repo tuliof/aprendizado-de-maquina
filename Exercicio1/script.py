f35edc5fcdcb71ae6ab12e2f9d07e2ad62a80a99
Tulio Fernandes <tulio.fernandes@gmail.com>
2014-03-17 01:19:56 -0300

    - Adicionado links no arquivo recursos.md  - Alterado script.py, de agora em diante irá usar matriz nas operaçoes.  - Adicionando o arquivo matrix.txt, que é o conteúdo do csv representado    em matriz.

--- Exercicio1/script.py
+++ Exercicio1/script.py
@@ -1,5 +1,6 @@
 import csv
 import math
+from scipy import numpy
 
 class Entity:
 	a = -1
@@ -31,7 +32,7 @@ def readCsvToObject():
 	return entityList
 
 def readCsvToMatrix():
-	with open('dados2.csv', 'rb') as csvfile:
+	with open('dados1.csv', 'rb') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 		rowNum = 0
 		for row in spamreader:
@@ -41,22 +42,7 @@ def readCsvToMatrix():
 				continue
 			elif rowNum == 2:
 				matrix = [[rowNum,stringToFloat(row[0]),stringToFloat(row[1]),stringToFloat(row[2]),stringToFloat(row[3])]]
-
 			matrix.append([rowNum,stringToFloat(row[0]),stringToFloat(row[1]),stringToFloat(row[2]),stringToFloat(row[3])])
-	return matrix
-
-def listToMatrix(itemList):
-	matrix = [[0 for x in xrange(5)] for x in xrange(len(itemList)) ]
-	line = 0
-
-	for item in itemList:
-		matrix[line][0] = item.lineNumber
-		matrix[line][1] = item.a
-		matrix[line][2] = item.b
-		matrix[line][3] = item.c
-		matrix[line][4] = item.d
-		line += 1
-
 	return matrix
 
 def stringToFloat(stringFloat):
@@ -69,13 +55,40 @@ def stringToFloat(stringFloat):
 		return num
 
 # Removes the records with missing data and return a list with each records line number.
-def sanitize(entityList):
+def sanitize(matrix):
 	lineNumberList = []
-	for ent in entityList:
-		if math.isnan(ent.a) or math.isnan(ent.b) or math.isnan(ent.c) or math.isnan(ent.d):
-			lineNumberList.append(ent.lineNumber)
-			entityList.remove(ent)
+	for l in matrix:
+		if math.isnan(l[1]) or math.isnan(l[2]) or math.isnan(l[3]) or math.isnan(l[4]):
+			lineNumberList.append(l[0])
+			matrix.remove(l)
 	return lineNumberList
+
+
+def findMedian(matrix):
+	a = []
+	b = []
+	c = []
+	d = []
+	# Get each data period and calculate the median.
+	for l in matrix:
+		a.append(l[1])
+		b.append(l[2])
+		c.append(l[3])
+		d.append(l[4])
+	calculateMedian(a)
+	calculateMedian(b)
+	calculateMedian(c)
+	calculateMedian(d)
+
+'''
+The median of a data set is the data point above 
+which half of  the data sits and below which half 
+of the data sits - essentially, it's the "middle" 
+point in a data set.
+'''
+def calculateMedian(dataList):
+	sortedList = dataList.sort()
+	
 
 def test():
 	entityList = readCsv()
@@ -83,18 +96,20 @@ def test():
 	print matrix
 
 def doWork():
-	entityList = readCsv()
+	matrix = readCsvToMatrix()
 	# Print the first 5 entities
+	#print '1 - %s' % len(matrix)
 	print 'First 5 entities:'
-	for ent in entityList[:5]:
-		print 'Line %s: %s, %s, %s, %s' % (ent.lineNumber, ent.a, ent.b, ent.c, ent.d)
+	for l in matrix[:5]:
+		print 'Line %s: %s, %s, %s, %s' % (l[0], l[1], l[2], l[3], l[4])
 	
 	# Remove entities with empty attributes
-	removedEntities = sanitize(entityList)
+	removedEntities = sanitize(matrix)
 	print '\nLines with empty data (will not be considered): '
 	for r in removedEntities:
 		print r
-	#
+	#print '2 - %s' % len(matrix)
+	findMedian(matrix)
 
 def main():
 	#test()
