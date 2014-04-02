70a266450d87627f248c3f160e1fffb516f60d7c
Túlio Fernandes <tulio.fernandes@gmail.com>
2014-04-02 15:08:57 -0300

    Renomeado scripts dos exercícios anteriores para o padrão.

--- Exercicio2/ex2.py
+++ Exercicio2/ex2.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
+import pandas as pd
+import pylab as pl
 
 def read_data(folder_path):
 	from os import walk
@@ -17,7 +19,7 @@ def read_data(folder_path):
 		break
 	
 	for f in files:
-		print 'Processing: %s' % f
+		#print 'Processing: %s' % f
 		file_metadata = os.path.basename(f).split('.')
 		if file_metadata[3] != 'pgm':
 			break
@@ -42,21 +44,34 @@ def read_pgm_file(filename):
 def get_class(filename):
 	return int(os.path.basename(filename).split('.')[0][0])
 
-def predict(train_data, train_data_classes, test_data):
+def train(n, train_data, train_data_classes):
+	print 'Training with range %s' % n
+	clf = KNeighborsClassifier(n_neighbors=n, weights='uniform')
+	# Train
+	clf.fit(train_data, train_data_classes)
+	return clf
+
+def predict(clf, test_data, test_classes):
 	results = []
-	for n in range(1, 51, 2):
-		clf = KNeighborsClassifier(n_neighbors=n)
-		# Train
-		clf.fit(train_data, train_data_classes)
-		# Predict
-		preds = clf.predict(test_data)
-		accuracy = np.where(preds==test['high_quality'], 1, 0).sum() / float(len(test_data))
-		print "Neighbors: %d, Accuracy: %3f" % (n, accuracy)
+	# Predict
 
-		results.append([n, accuracy])
+	print test_classes
+	number_of_classes = len(test_classes)
 
+	for x in range(0, len(test_data)):
+		print test_data[x]
+		print 'Trying to predict number: %s' % test_classes[x]
+		preds = clf.predict(test_data[x])
+		print preds
+		preds = np.array(preds.sort())
+		#print np.where(preds==test_classes)
+		accuracy = np.where(preds==np.array(test_classes.sort()), 1, 0).sum() / float(number_of_classes)
+		#print "Neighbors: %d, Accuracy: %3f" % (n, accuracy)
+		#results.append([n, accuracy])
+	
+def plot_result(results):
+	# Plot results
 	results = pd.DataFrame(results, columns=["n", "accuracy"])
-
 	pl.plot(results.n, results.accuracy)
 	pl.title("Accuracy with Increasing K")
 	pl.show()
@@ -67,14 +82,26 @@ def main():
 	# Read data from file
 	
 	# LEARN
+	print 'Processing training files...'
 	train_data = read_data(train_folder)
 	# PREDICT
+	print 'Processing testing files...'
 	test_data = read_data(test_folder)
 
 	np_test = np.array(test_data[0])
+	np_test_classes = np.array(test_data[1])
 	np_train = np.array(train_data[0])
 	np_classes = np.array(train_data[1])
-	clf = predict(np_train, np_classes, np_test)
+	results = []
+
+	#for n in range(1, 51, 2):
+	clf = train(23, np_train, np_classes)
+	predict(clf, np_test, np_test_classes)
+
+	print 'Test data classes'
+	print test_data[1]
+
+	#plot_result(results)
 
 if __name__ == "__main__":
 	main()
